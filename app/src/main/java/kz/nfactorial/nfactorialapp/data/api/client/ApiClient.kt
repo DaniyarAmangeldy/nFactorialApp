58ac54569321bebde9d0ebf266f00bdf725b3674
daniyar.amangeldy <d.amangeldy@arbuz.kz>
2024-12-23 17:35:08 +0500

    Lesson 12 - Networking. OkHttp

--- app/src/main/java/kz/nfactorial/nfactorialapp/data/api/client/ApiClient.kt
+++ app/src/main/java/kz/nfactorial/nfactorialapp/data/api/client/ApiClient.kt
@@ -1,11 +1,7 @@
 package kz.nfactorial.nfactorialapp.data.api.client
 
 import java.util.concurrent.TimeUnit
-import kotlinx.serialization.json.Json
-import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
-import retrofit2.Retrofit
-import retrofit2.converter.kotlinx.serialization.asConverterFactory
 
 class ApiClient {
 
@@ -14,14 +10,6 @@ class ApiClient {
             .Builder()
             .readTimeout(60_000, TimeUnit.MILLISECONDS)
             .writeTimeout(60_000, TimeUnit.MILLISECONDS)
-            .build()
-    }
-
-    val retrofit by lazy {
-        Retrofit.Builder()
-            .baseUrl("https://nfactorialappbackend.onrender.com/")
-            .client(client)
-            .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
             .build()
     }
 }
